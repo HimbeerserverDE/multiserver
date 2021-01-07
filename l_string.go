b865f60079d6a95518217c545eae6b634a97fdd0
HimbeerserverDE <lpaschek@icloud.com>
2021-01-07 12:30:27 +0100

    Fix multiserver.split

--- l_string.go
+++ l_string.go
@@ -6,11 +6,11 @@ import (
 	"github.com/yuin/gopher-lua"
 )
 
-func luaStringSplit(L *lua.LState) {
+func luaStringSplit(L *lua.LState) int {
 	s := L.ToString(1)
 	d := L.ToString(2)
 	
-	L.Push(lua.LString(strings.Split(s, d)))
+	L.Push(lua.LString(strings.Join(strings.Split(s, d), "")))
 	
 	return 1
 }
