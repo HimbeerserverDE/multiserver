a276e639bed210a14cebcbf96e2fed7e3deba967
HimbeerserverDE <himbeerserverde@gmail.com>
2021-03-10 19:17:36 +0100

    Handle SIGTERM

--- signal.go
+++ signal.go
@@ -3,12 +3,13 @@ package main
 import (
 	"os"
 	"os/signal"
+	"syscall"
 )
 
 func init() {
 	go func() {
 		signalChan := make(chan os.Signal, 1)
-		signal.Notify(signalChan, os.Interrupt)
+		signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
 		<-signalChan
 
 		End(false, false)
