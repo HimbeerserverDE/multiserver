e72c63931081d9eb17ffb543862226642d53315f
HimbeerserverDE <himbeerserverde@gmail.com>
2021-04-22 10:57:47 +0200

    Expand DB API

--- db.go
+++ db.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"strings"
 
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
@@ -31,7 +32,7 @@ func OpenSQLite3(name, initSQL string) (*DB, error) {
 }
 
 // OpenPSQL opens and returns a PostgreSQL database
-func OpenPSQL(host, name, user, password string, port uint16) (*DB, error) {
+func OpenPSQL(host, name, user, password, initSQL string, port int) (*DB, error) {
 	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, name)
 
 	db, err := sql.Open("postgres", psqlconn)
@@ -45,10 +46,23 @@ func OpenPSQL(host, name, user, password string, port uint16) (*DB, error) {
 		return nil, err
 	}
 
+	if _, err := db.Exec(initSQL); err != nil {
+		db.Close()
+		return nil, err
+	}
+
 	return &DB{DB: db}, nil
+}
+
+// Exec executes a SQL statement
+func (db *DB) Exec(sql string, values ...interface{}) error {
+	sql = strings.ReplaceAll(sql, "?", "$x")
+	_, err := db.DB.Exec(sql, values)
+	return err
 }
 
 // Query executes a SQL statement and stores the results
 func (db *DB) QueryRow(sql string, values []interface{}, results ...interface{}) error {
+	sql = strings.ReplaceAll(sql, "?", "$x")
 	return db.DB.QueryRow(sql, values...).Scan(results...)
 }
